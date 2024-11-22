6eb77bbbaae8c2943dbedbf0c1fe2ddc5eddf163
cg-223 <cg223celeste@gmail.com>
2024-11-22 17:49:38 -0500

    add loader
    priority dictated by name
    if names are as follows
    1.lua
    100.lua
    123.lua
    20.lua
    load order is as follows
    100.lua
    123.lua
    1.lua
    20.lua

---
+++ loader.lua
@@ -0,0 +1,25 @@
+--metadata @ steamodded-metadata.json
+local function sort_string_helper(a, b)
+    for i = 1, ((#a > #b) and #b) or #a do
+        if string.byte(string.sub(a, i, i)) < string.byte(string.sub(b, i, i)) then
+            return true
+        elseif string.byte(string.sub(a, i, i)) > string.byte(string.sub(b, i, i)) then
+            return false
+        end
+    end
+    return ((#a < #b) and true) or false
+end
+
+
+local path = SMODS.current_mod.path
+path = path .. "modules/"
+
+local modules = NFS.getDirectoryItems(path)
+table.sort(modules, sort_string_helper)
+
+--sort the modules to load in alphabetical order (theyre all numbers, followed by a set amount of underscores)
+
+for i, v in pairs(modules) do
+    local pth = path .. v
+    loadfile(pth) ()
+end
